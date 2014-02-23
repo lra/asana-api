bf358ec6bf7cd1b7673b284d9a8a0e6cad8358bc
Laurent Raufaste <analogue@glop.org>
2014-02-22 20:20:09 -0500

    Initial setup file to register the package name

---
+++ setup.py
@@ -0,0 +1,11 @@
+"""Setup file to automate the install of asana-api in a Python environment"""
+import distutils.core
+
+
+distutils.core.setup(
+    name='asana-api',
+    version='0.1',
+    author='Laurent Raufaste',
+    author_email='analogue@glop.org',
+    url='https://github.com/lra/asana-api',
+)
